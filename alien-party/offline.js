daf48985c15d0d49666e1e492f40b80c97ea9c06
Carlos Campaña <campinya@hotmail.com>
2016-11-04 12:38:34 +0100

    Alien party

---
+++ alien-party/offline.js
@@ -0,0 +1,19 @@
+﻿{
+	"version": 1478259480,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"images/bomba-sheet0.png",
+		"images/luchador-sheet0.png",
+		"images/controljugador-sheet0.png",
+		"images/cronometrobomba.png",
+		"images/explosion.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
